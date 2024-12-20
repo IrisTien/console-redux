16c83d7aa5050b770215a5bfbef8d0ca848c0409
Wei Tian <twei@vmware.com>
2024-12-20 15:04:26 +0800

    First Commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { createStore, applyMiddleware } = require('redux');
+
 const defaultState = {
   courses: [
     {
@@ -11,12 +13,35 @@ const defaultState = {
     {
       name: 'Using Redux with Angular',
       topic: 'Angular',
-    }
-  ]
+    },
+  ],
 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_COURSE':
+      return Object.assign({}, state, {
+        courses: [...state.courses, action.course],
+      });
+    default:
+      return state;
+  }
+}
+
+const logger = (store) => (next) => (action) => {
+  console.log('dispatching ', action);
+  const result = next(action);
+  console.log('state after action', store.getState());
+  return result;
+};
+
+const store = createStore(reducer, defaultState, applyMiddleware(logger));
 
 function addView(viewFunc) {
   viewFunc(defaultState);
+  store.subscribe(() => {
+    viewFunc(store.getState());
+  });
 }
 
 addView((state) => {
@@ -24,9 +49,17 @@ addView((state) => {
 });
 
 addView((state) => {
-  console.log(`The latest course in the library: ${state.courses[state.courses.length -1].name}`);
+  console.log(
+    `The latest course in the library: ${
+      state.courses[state.courses.length - 1].name
+    }`
+  );
 });
 
-
-
-
+store.dispatch({
+  type: 'ADD_COURSE',
+  course: {
+    name: 'Learning Redux Test',
+    topic: 'Learning redux first lesson',
+  },
+});
